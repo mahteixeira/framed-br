fdf63104f99f5aa3265ac16e50ebfd21b6e5199d
mahteixeira <teixeiramarina13@gmail.com>
2025-08-28 00:06:23 -0300

    novos filmes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // total de pastas (ajuste aqui se tiver mais ou menos)
-const totalPastas = 1;
+const totalPastas = 3;
 
 // gera número aleatório entre 1 e totalPastas
 const numero = Math.floor(Math.random() * totalPastas) + 1;
@@ -23,22 +23,22 @@ function mudarFrame(num) {
     const frame = document.getElementById("imagem");
 
     if (num === 1) {
-        frame.src = "imagens/1/frame1.jpeg";
+        frame.src = "imagens/${numero}/frame1.jpeg";
     }
     else if (num === 2) {
-        frame.src = "imagens/1/frame2.jpeg";
+        frame.src = "imagens/${numero}/frame2.jpeg";
     }
     else if (num === 3) {
-        frame.src = "imagens/1/frame3.jpeg";
+        frame.src = "imagens/${numero}/frame3.jpeg";
     }
     else if (num === 4) {
-        frame.src = "imagens/1/frame4.jpeg";
+        frame.src = "imagens/${numero}/frame4.jpeg";
     }
     else if (num === 5) {
-        frame.src = "imagens/1/frame5.jpeg";
+        frame.src = "imagens/${numero}/frame5.jpeg";
     }
     else if (num === 6) {
-        frame.src = "imagens/1/frame6.jpeg";
+        frame.src = "imagens/${numero}/frame6.jpeg";
     }
     
 }
@@ -165,6 +165,7 @@ function validarResposta() {
 
   if (inputresposta === respotacerta) {
     alert("RESPOSTA CERTA");
+    location.reload();
   } else {
     alert("RESPOSTA ERRADA, TENTE NOVAMENTE");
     mostrarProximo();
